77b38f9480a4fb9d6fb090e20905843325173511
Arts M J <115874148+souledspirit@users.noreply.github.com>
2026-02-04 13:52:05 +0530

    custom middleware

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+
+const logger = (req, res, next) => {
+  console.log(`${req.method}  request to ${req.url}`);
+  next();
+};
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -12,11 +22,31 @@ app.get("/api/data", (req, res) => {
   res.json({ message: "this is the response form the server test 1" });
 });
 
-app.post("/api/send-data", (req, res) => {
-  res.header("Content-Type:application/json");
-  res.status(200);
-  res.json({ message: "data received sucessfully" });
+app.set("view engine", "ejs");
+
+app.get("/home", (req, res) => {
+  res.render("index");
 });
+
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+app.post("/api/submit-form", (req, res) => {
+  const formData = req.body;
+  res.render("sucess", { data: req.body });
+});
+
+app.post("/api/send-data", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  res.json({ message: "data received successfully" });
+});
+
+console.log(path.basename(__filename) + " is running...");
+console.log("os.platform(): " + require("os").platform());
+console.log();
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
